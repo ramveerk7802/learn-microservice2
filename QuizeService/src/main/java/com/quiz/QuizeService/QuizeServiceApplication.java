e3b62b64803ae18f5d11f5be6aa394a3ac50df78
Ramveer <ramveerk7802@gmail.com>
2025-09-02 22:55:47 +0530

    initial setup project of microservice(Question,Quiz service)

---
+++ QuizeService/src/main/java/com/quiz/QuizeService/QuizeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.QuizeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizeServiceApplication.class, args);
+	}
+
+}
